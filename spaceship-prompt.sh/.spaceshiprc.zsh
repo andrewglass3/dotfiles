40f69eeacb52109d1803d6d4e23d0cf7e58b39fc
Andrew Glass <andrew.glass@outlook.com>
2024-07-25 17:58:08 +0100

    added initial spaceship-prompt.sh config file

---
+++ spaceship-prompt.sh/.spaceshiprc.zsh
@@ -0,0 +1,21 @@
+# Display time
+SPACESHIP_TIME_SHOW=true
+
+# Display username always
+SPACESHIP_USER_SHOW=always
+
+# Do not truncate path in repos
+SPACESHIP_DIR_TRUNC_REPO=false
+
+SPACESHIP_TERRAFORM_SHOW=true
+
+#spaceship remove git
+spaceship add terraform
+spaceship add aws
+spaceship add host
+spaceship add python
+spaceship add battery
+
+# Add custom Ember section
+# See: https://github.com/spaceship-prompt/spaceship-ember
+#spaceship add ember
